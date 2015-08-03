a44480414b277dfa6e90d3c4f5741eac7f6376eb
Jim Rutherford <jim@taptonics.com>
2015-08-03 13:53:14 -0700

    Configuring cells

--- TPTSwipingTableViewCell/TableViewController.swift
+++ TPTSwipingTableViewCell/TableViewController.swift
@@ -34,12 +34,61 @@ class TableViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("swipeCell", forIndexPath: indexPath)
+        let cell:TPTSwipingTableViewCell = tableView.dequeueReusableCellWithIdentifier("swipeCell", forIndexPath: indexPath) as! TPTSwipingTableViewCell
 
         cell.textLabel!.text = tableData[indexPath.row]
 
+        configureCell(cell, forRowAtIndexPath:indexPath)
+        
         return cell
     }
-
-
+    
+    func configureCell(cell:TPTSwipingTableViewCell, forRowAtIndexPath:NSIndexPath)
+    {
+        cell.defaultColor = UIColor.lightGrayColor()
+        
+        let commentView = viewWithImageName("comment")
+        let configView = viewWithImageName("config")
+        let heartView = viewWithImageName("heart")
+        let tagView = viewWithImageName("tag")
+        
+        let greenColor = UIColor(colorLiteralRed:0.33, green:0.84, blue:0.31, alpha:1.0)
+        let redColor = UIColor(colorLiteralRed:0.91, green:0.24, blue:0.05, alpha:1.0)
+        let yellowColor = UIColor(colorLiteralRed:1.0, green:0.85, blue:0.22, alpha:1.0)
+        let brownColor = UIColor(colorLiteralRed:0.81, green:0.58, blue:0.38, alpha:1.0)
+        
+        cell.setSwipeGestureWithView(commentView, color: greenColor, mode: .Switch, state: .State1, completionBlock: { (cell, state, mode) -> Void in
+            
+            
+            
+        })
+        
+        cell.setSwipeGestureWithView(configView, color: redColor, mode: .Switch, state: .State2, completionBlock: { (cell, state, mode) -> Void in
+            
+            
+            
+        })
+        
+        cell.setSwipeGestureWithView(heartView, color: yellowColor, mode: .Switch, state: .State3, completionBlock: { (cell, state, mode) -> Void in
+            
+            
+            
+        })
+        
+        cell.setSwipeGestureWithView(tagView, color: brownColor, mode: .Switch, state: .State4, completionBlock: { (cell, state, mode) -> Void in
+            
+            
+            
+        })
+        
+    }
+    
+    
+    func viewWithImageName(imageName:String) -> UIView {
+        let image = UIImage(named:imageName)
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .Center
+        return imageView
+    }
+    
 }
